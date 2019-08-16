a5690a5b6c19ac70f92240353037009d2cf7ba27
Park Chan Wan <pchanwan@naver.com>
2019-08-16 22:26:56 +0900

    2019.Aug.16 11:26PM

---
+++ DP/PurchasingCard.py
@@ -0,0 +1,19 @@
+'''
+백준 11052 - 카드구매
+'''
+
+import sys
+
+N = int(sys.stdin.readline())
+temp = list(map(int, sys.stdin.readline().split()))
+array = [0 for _ in range(N + 1)]
+
+for i in  range(1, N + 1) :
+    array[i] = temp[i - 1]
+
+for i in range(1, N + 1) :
+    for j in range(i + 1, N + 1) :
+        if array[j] < array[j-i] + array[i] :
+            array[j] = array[j-i] + array[i]
+
+print(array[N])
