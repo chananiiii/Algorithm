4076078c68e3f111173c9fb64572a5de652cd06d
Park Chan Wan <pchanwan@naver.com>
2019-08-17 16:14:18 +0900

    2019.Aug.17 5:14PM

---
+++ ETC/AMinusB.py
@@ -0,0 +1,8 @@
+'''
+백준 1001번 A-B
+'''
+
+import sys
+temp = list(map(int, sys.stdin.readline().split()))
+print(temp[0] - temp[1])
+
---
+++ ETC/APlusB.py
@@ -0,0 +1,6 @@
+'''
+백준 1000번 A+B
+'''
+
+import sys
+print(sum(list(map(int, sys.stdin.readline().split()))))
